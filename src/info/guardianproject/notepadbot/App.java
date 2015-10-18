0384549c760685ab4b3d5a95e5683a5e4536274c
Jonathan Coe <jcoe@protonmail.ch>
2015-10-18 14:52:44 +0100

    Moved PRNGFixes.apply() statement to App.java and deleted the now redundant file NoteCipherApp.java

--- src/info/guardianproject/notepadbot/App.java
+++ src/info/guardianproject/notepadbot/App.java
@@ -13,6 +13,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Apply the Google PRNG fixes to properly seed SecureRandom
+        PRNGFixes.apply();
+
         mCWSettings = new CacheWordSettings(getApplicationContext());
         mCWSettings.setNotificationIntent(PendingIntent.getActivity(getApplicationContext(), 0, new Intent(this, NoteCipher.class), Intent.FLAG_ACTIVITY_NEW_TASK ));
     }
