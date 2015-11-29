a5e3702f1c38d668bf42f96ee718dd065bf5d850
Jonathan Coe <jcoe@protonmail.ch>
2015-11-29 18:14:52 +0000

    Corrected a typo

--- src/io/darknote/NConstants.java
+++ src/io/darknote/NConstants.java
@@ -6,12 +6,12 @@ public class NConstants {
 
 	public static final int MAX_STREAM_SIZE = 1000000;
 	public static final int MIN_PASS_LENGTH = 8;
-    // public final static int MAX_PASS_ATTEMPTS = 3;
-    // public final static int PASS_RETRY_WAIT_TIMEOUT = 30000;
 	 
 	/**
-	 * Checks if the password is valid based on it's length
+	 * Checks if the password is valid based on its length
+     *
 	 * @param pass
+     *
 	 * @return True if the password is a valid one, false otherwise
 	 */
 	public static final boolean validatePassword(char[] pass) {
